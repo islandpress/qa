eb34623258fcdb475e1a0dec43fbe25731b1c4c6
T-Dnzt <thi.denizet@gmail.com>
2017-07-15 11:49:08 +0700

    Design for user views

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,10 +1,8 @@
 module TagsHelper
 
-  def tag_schema(name,count=nil)
-    html = "<div class=\"tag #{Tag.special?(name)}\">"
-    html << link_to(name,tag_path(:id=>name))
-    html << "<span><strong> x #{count}</strong></span>" if count
-    html << "</div>"
+  def tag_schema(name, count=nil)
+    computed_name = count ? "##{name} (#{count})" : "##{name}"
+    html = link_to(computed_name, tag_path(id: name), class: 'tags-list__tag')
     raw html
   end
 
